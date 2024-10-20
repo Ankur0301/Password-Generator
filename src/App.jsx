c96fea3108dae6b0b518627b125308a70ac9b408
Ankur Agrawal <agrank1020@gmail.com>
2024-10-20 23:45:46 +0530

    Refactor password generation logic and fix initial state bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useCallback } from "react";
 
 function App() {
-  const [length, setLength] = useState[8];
+  const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [characterAllowed, setCharacterAllowed] = useState(false);
   const [password, setPassword] = useState("");
@@ -10,9 +10,9 @@ function App() {
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     if (numberAllowed) str += "0123456789";
     if (characterAllowed) str += "!@#$%^&*()_+";
-    for (let i = 0; i < array.length; i++) {
-      let char = Math.floor(Math.random() * str.length + 1);
-      pass = str.charAt(char);
+    for (let i = 0; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length);
+      pass += str.charAt(char);
     }
     setPassword(pass);
   }, [length, numberAllowed, characterAllowed, setPassword]);
@@ -21,7 +21,7 @@ function App() {
     <>
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 border-gray-700 ">
         <h1 className="text-4xl p-6 text-center text-black">
-          Password Generator 1
+          Password Generator
         </h1>
       </div>
     </>
