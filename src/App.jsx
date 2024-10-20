bf59c1970291e91af7c584f4f59024c3ddab3e2e
Ankur Agrawal <agrank1020@gmail.com>
2024-10-20 23:37:29 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
     <>
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 border-gray-700 ">
         <h1 className="text-4xl p-6 text-center text-black">
-          Password Generator
+          Password Generator 1
         </h1>
       </div>
     </>
